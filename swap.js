2e20a1f884df6c27ffd1e3c4b9037fa347110cb3
gengxx <geng@xx.com>
2021-05-20 18:13:23 +0800

    unisdk 初始化

---
+++ swap.js
@@ -0,0 +1,9 @@
+// ES6语法
+// import { ChainId } from '@uniswap/sdk'
+// console.log(`The chainId of mainnet is ${ChainId.MAINNET}.`)
+
+// CommonJS语法 NODE使用
+// const UNISWAP = require('@uniswap/sdk')
+// console.log(`The chainId of mainnet is ${UNISWAP.ChainId.MAINNET}.`)
+
+
